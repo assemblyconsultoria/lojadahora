ef2bd249a6e2131f6536206d392f6e6dd25e6f13
Assembly Consultoria <assembly_consultoria@proton.me>
2026-02-23 17:54:08 -0300

    Criação do projeto Loja da Hora

---
+++ src/main/java/br/com/lojadahora/lojadahora/LojadahoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojadahora.lojadahora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadahoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadahoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lojadahora/lojadahora/LojadahoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojadahora.lojadahora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojadahoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
